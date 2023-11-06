0d00a8fdc06f92309f13f266e929c42b4f20dee5
georgeasnts <georgea.freitas1@gmail.com>
2023-11-06 14:50:51 -0500

    fizzbuzz0 included script
    Now the code is changing the h2 whenever the user insert the information and press submit. Reset button not working, touppercase not working.

--- web115/scripts/fizzbuzz0.js
+++ web115/scripts/fizzbuzz0.js
@@ -1,2 +1,21 @@
-let fname = document.getElementById('first_name').value;
-console.log(first_name);
+document.addEventListener("DOMContentLoaded", function() {
+	document.getElementById('submit').addEventListener('click', function(event) {
+	    event.preventDefault(); // Prevent the form from submitting and page reload
+    
+	    // Get the values entered in the form fields
+	    let firstName = document.getElementById('first_name').value;
+	    let middleName = document.getElementById('middle_initial').value;
+	    let lastName = document.getElementById('last_name').value;
+    
+	    // Combine the values to create the user's full name
+	    let fullName = `${firstName} ${middleName}. ${lastName}`;
+    
+	    // Create a new greeting message using the user's full name
+	    let newGreeting = `Welcome to User Utopia, ${fullName}!`;
+    
+	    // Update the greeting displayed on the page with the new greeting
+	    document.getElementById('greeting').textContent = newGreeting;
+	});
+
+    });
+    
