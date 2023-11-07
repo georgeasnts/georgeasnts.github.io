b7b9d3638232ab21f8bf4d6b18a3e4c3eb7fcd98
georgeasnts <georgea.freitas1@gmail.com>
2023-11-06 22:15:02 -0500

    Fixed reset button erros

--- web115/scripts/fizzbuzz0.js
+++ web115/scripts/fizzbuzz0.js
@@ -1,21 +1,32 @@
-document.addEventListener("DOMContentLoaded", function() {
-	document.getElementById('submit').addEventListener('click', function(event) {
-	    event.preventDefault(); // Prevent the form from submitting and page reload
+// sets the original greeting as a constant variable
+let originalGreeting = document.getElementById('greeting').textContent;
+//adds an event listener to the submit button when clicked
+document.getElementById('submit').addEventListener('click', function(event) {
     
-	    // Get the values entered in the form fields
-	    let firstName = document.getElementById('first_name').value;
-	    let middleName = document.getElementById('middle_initial').value;
-	    let lastName = document.getElementById('last_name').value;
-    
-	    // Combine the values to create the user's full name
-	    let fullName = `${firstName} ${middleName}. ${lastName}`;
-    
-	    // Create a new greeting message using the user's full name
-	    let newGreeting = `Welcome to User Utopia, ${fullName}!`;
-    
-	    // Update the greeting displayed on the page with the new greeting
-	    document.getElementById('greeting').textContent = newGreeting;
-	});
+	//gets the values for the input fields
+    let firstName = document.getElementById('first_name').value;
+    let middleName = document.getElementById('middle_initial').value;
+    let lastName = document.getElementById('last_name').value;
 
-    });
-    
+     //capitalizes the first letter of 
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+    }
+
+    firstName = capitalizeFirstLetter(firstName);
+    middleName = capitalizeFirstLetter(middleName);
+    lastName = capitalizeFirstLetter(lastName);
+
+    //ceates a full name by combining the inputs
+    let fullName = `${firstName} ${middleName}. ${lastName}`;
+    // creates a new greeting with the created full name
+    let newGreeting = `Welcome to User Utopia, ${fullName}!`;
+	//updates the greeting that appears on the page
+    document.getElementById('greeting').textContent = newGreeting;
+});
+
+//adds an event listener to the reset button when clicked
+document.getElementById('reset').addEventListener('click', function() {
+	//gets the greeting back to the original text
+    document.getElementById('greeting').textContent = originalGreeting;
+});
