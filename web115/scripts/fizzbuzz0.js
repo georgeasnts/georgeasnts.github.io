9882912c458f1d0d1fa1b3aadb8d0509060a6cde
georgeasnts <georgea.freitas1@gmail.com>
2023-12-09 12:12:21 -0500

    ignore
    ignore

--- web115/scripts/fizzbuzz0.js
+++ web115/scripts/fizzbuzz0.js
@@ -25,29 +25,26 @@ document.getElementById('submit').addEventListener('click', function(event) {
     document.getElementById('greeting').textContent = newGreeting;
     alert(`Welcome, ${fullName}!`);
    
+    /*// Loop to output 125 lines
+    let outputList = document.getElementById('outputList');
+    outputList.innerHTML = ''; // Clear previous content*/
 
     // promtp to aget user input
     let userNumber = prompt(`How high do you want to count, ${firstName}?`);
+    // Convert the input to an integer
+   // Loop to output userNumber lines related to the theme
+for (let i = 1; i <= userNumber; i++) {
+    let themeWord1 = "User";
+    let themeWord2 = "Utopia";
+    let line = `${i}. ${themeWord1} ${themeWord2} - the number is ${i % 2 === 0 ? 'even' : 'odd'}`;
 
-    /* Loop to output 125 lines
-    let outputList = document.getElementById('outputList');
-    outputList.innerHTML = '';  Clear previous content*/
-    
-    // Loop to output user input
-    for (let i = 1; i <= userNumber; i++) {
-        let themeWord1 = "User";
-        let themeWord2 = "Utopia";
-        let line = `${i}. ${themeWord1} ${themeWord2} - the number is ${i % 2 === 0 ? 'even' : 'odd'}`;
-    
-        // Create a new list item
-        let listItem = document.createElement('li');
-        listItem.textContent = line;
-    
-        // outputs the new list item
-        outputList.innerHTML = '';
-        outputList.appendChild(listItem);
-    }
+    // Create a new list item and append to the ordered list
+    let listItem = document.createElement('li');
+    listItem.textContent = line;
+    outputList.appendChild(listItem);
+}
 
+  
 });
 
 document.getElementById('reset').addEventListener('click', function() {
