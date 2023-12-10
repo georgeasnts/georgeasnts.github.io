a774eb9486fb08a594a4d4a00a68208d94eb4a22
georgeasnts <georgea.freitas1@gmail.com>
2023-12-10 11:16:00 -0500

    FizzBuzz1
    Copies fizzbuzz0, updated to match fizzbuzz1 conditions

---
+++ web115/scripts/fizzbuzz1.js
@@ -0,0 +1,62 @@
+// sets the original greeting as a constant variable
+let originalGreeting = document.getElementById('greeting').textContent;
+
+//adds an event listener to the submit button when clicked
+document.getElementById('submit').addEventListener('click', function(event) {
+    //gets the values for the input fields
+    let firstName = document.getElementById('first_name').value;
+    let middleName = document.getElementById('middle_initial').value;
+    let lastName = document.getElementById('last_name').value;
+
+    //capitalizes the first letter of 
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+    }
+
+    firstName = capitalizeFirstLetter(firstName);
+    middleName = capitalizeFirstLetter(middleName);
+    lastName = capitalizeFirstLetter(lastName);
+
+    //creates a full name by combining the inputs
+    let fullName = `${firstName} ${middleName}. ${lastName}`;
+    //creates a new greeting with the created full name
+    let newGreeting = `Welcome to User Utopia, ${fullName}!`;
+
+    //updates the greeting that appears on the page
+    document.getElementById('greeting').textContent = newGreeting;
+    alert(`Welcome, ${fullName}!`);
+
+    // Clear previous content
+    let outputList = document.getElementById('outputList');
+    outputList.innerHTML = '';
+
+    // Loop to output 140 lines with the same word (in this case, "Aesthetic")
+    let themeWord = "Aesthetic";
+    for (let i = 1; i <= 140; i++) {
+        let themeWord = '';
+        if (i % 3 === 0) {
+            themeWord += "Appealing ";
+        }
+        if (i % 5 === 0) {
+            themeWord += "Elegant";
+        }
+        if (!themeWord) {
+            themeWord = "Aesthetic";
+        }
+
+        let line = `${i}. ${themeWord}`;
+        
+        // Create a new list item and append to the ordered list
+        let listItem = document.createElement('li');
+        listItem.textContent = line;
+        outputList.appendChild(listItem);
+    }
+});
+
+document.getElementById('reset').addEventListener('click', function() {
+    // Reset the greeting to the original text
+    document.getElementById('greeting').textContent = originalGreeting;
+
+    // Clear the content of the ordered list
+    document.getElementById('outputList').innerHTML = '';
+});
